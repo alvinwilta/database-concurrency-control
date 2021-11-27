248e621637ad03b460b6b179ec4956029b6086dd
alvinwilta <13519163@std.stei.itb.ac.id>
2021-11-27 10:46:18 +0700

    adding comments

--- mvcc.py
+++ mvcc.py
@@ -1,11 +1,16 @@
+# Dibuat oleh Alvin Wilta 13519163
 class Transaction:
+    '''
+    # Kelas transaksi, untuk mendefinisikan struct dari transaksi
+    op      : list of dict dari operasi yang dilakukan
+    time    : timestamp dari transaksi
+    curr_op : pointer untuk mengakses operasi dan untuk rollback
+    '''
+
     def __init__(self, op: list, time: int):
         self.op = op
         self.time = time
         self.curr_op = 0
-
-    def at(el: int):
-        return self.op[el]
 
 
 class Op:
@@ -15,6 +20,7 @@ class Op:
     C = 'commit'
 
 
+# Asumsi operasi yang dilakukan t1 akan terurut dari indeks 0 hingga akhir
 op = [{Op.R: 'A'}, {Op.R: 'B'}, {Op.W: 'B'}]
 
 t1 = Transaction(op, 1)
