55be3fa169d1595aa59ac2696e143978b7259636
alvinwilta <13519163@std.stei.itb.ac.id>
2021-11-27 10:38:54 +0700

    test structure for transaction

--- mvcc.py
+++ mvcc.py
@@ -1,8 +1,8 @@
 class Transaction:
-    def __init__(self, name: str, op: list, time: int):
-        self.name = name
+    def __init__(self, op: list, time: int):
         self.op = op
         self.time = time
+        self.curr_op = 0
 
     def at(el: int):
         return self.op[el]
@@ -13,3 +13,11 @@ class Op:
     R = 'read'
     W = 'write'
     C = 'commit'
+
+
+op = [{Op.R: 'A'}, {Op.R: 'B'}, {Op.W: 'B'}]
+
+t1 = Transaction(op, 1)
+
+for x in t1.op:
+    print(x)
