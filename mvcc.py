14115f019664d1fd416f5a6b09bd7ad04e1636db
alvinwilta <13519163@std.stei.itb.ac.id>
2021-11-27 20:07:42 +0700

    separate files, enhance pretty print, add comments

--- mvcc.py
+++ mvcc.py
@@ -1,72 +1,9 @@
-# Environment untuk simulasi transaksi dbms (input & data structure)
-class Transaction:
-    '''
-    # Kelas transaksi, untuk mendefinisikan struct dari transaksi
-    id      : id transaksi
-    op      : jenis operasi (R/W/C)
-    res     : resource yang diakses
-    '''
+import utils as u
 
-    def __init__(self, id: int, op: str, res: str):
-        self.id = id
-        self.op = op
-        self.res = res
-
-    def pr():
-        print('test')
-        return (f'id:{self.id} op:{self.op} res:{self.res}')
-
-
-R = 'R'
-W = 'W'
-C = 'C'
-
-
-def createOp(op: str, res: str):
-    return {'op': op, 'res': res}
-
-
-def createTransaction():
-    n_commit = 0
-    tmp = []
-    trans_list = []
-    is_fin = False
-    while (not (is_fin)):
-        inp = input('specify operations (R/W/C) ex. R1: ')
-        opr = inp[0]
-        id = int(inp[1])
-        if (not(opr == C)):
-            res = input('specify resource for operations: ')
-        if (id not in trans_list):
-            trans_list.append(id)
-        if (opr == R or opr == W):
-            tmp.append(Transaction(id=id, op=opr, res=res))
-        elif (opr == C):
-            n_commit += 1
-            tmp.append(Transaction(id=id, op=C, res='commit'))
-
-        else:
-            print('invalid operations! ')
-        if(len(trans_list) == n_commit):
-            is_fin = True
-
-    return tmp, trans_list
-
-
-def prettyPrint(t: list):
-    for x in t:
-        if (x.op == C):
-            print("C" + str(x.id) + "->", end="")
-        else:
-            print(f'{x.op}{str(x.id)}({x.res})->', end="")
-    print()
-
-
-# Asumsi operasi yang dilakukan t1 akan terurut dari indeks 0 hingga akhir
-x, y = createTransaction()
+x, y = u.createTransaction()
 
 for el in x:
     print(f'id:{el.id} op:{el.op} res:{el.res}')
 print(y)
 
-prettyPrint(x)
+u.prettyPrint(x)
---
+++ utils.py
@@ -0,0 +1,63 @@
+# Environment untuk simulasi transaksi dbms (input & data structure)
+
+R = 'R'
+W = 'W'
+C = 'C'
+
+
+class Transaction:
+    '''
+    # Kelas transaksi, untuk mendefinisikan struct dari transaksi
+    id      : id transaksi
+    op      : jenis operasi (R/W/C)
+    res     : resource yang diakses
+    '''
+
+    def __init__(self, id: int, op: str, res: str):
+        self.id = id
+        self.op = op
+        self.res = res
+
+    def pr():
+        print('test')
+        return (f'id:{self.id} op:{self.op} res:{self.res}')
+
+
+def createTransaction():
+    '''
+    Function for inputting transaction data
+    '''
+    n_commit = 0
+    tmp = []
+    trans_list = []
+    is_fin = False
+    while (not (is_fin)):
+        inp = input('specify operations (R/W/C) ex. R1: ')
+        opr = inp[0]
+        id = int(inp[1])
+        if (not(opr == C)):
+            res = input('specify resource for operations: ')
+        if (id not in trans_list):
+            trans_list.append(id)
+        if (opr == R or opr == W):
+            tmp.append(Transaction(id=id, op=opr, res=res))
+        elif (opr == C):
+            n_commit += 1
+            tmp.append(Transaction(id=id, op=C, res='commit'))
+
+        else:
+            print('invalid operations! ')
+        if(len(trans_list) == n_commit):
+            is_fin = True
+
+    return tmp, trans_list
+
+
+def prettyPrint(t: list):
+    ret = ''
+    for x in t:
+        if (x.op == C):
+            ret += ("C" + str(x.id) + "->")
+        else:
+            ret += (x.op + str(x.id) + '(' + x.res + ')->')
+    print(ret[:-2])
