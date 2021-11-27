c4dc9ad9d21644398769f8be1d094faee0fd695a
alvinwilta <13519163@std.stei.itb.ac.id>
2021-11-27 19:26:36 +0700

    transaction data structure

--- mvcc.py
+++ mvcc.py
@@ -1,4 +1,4 @@
-# Dibuat oleh Alvin Wilta 13519163
+# Environment untuk simulasi transaksi dbms (input & data structure)
 class Transaction:
     '''
     # Kelas transaksi, untuk mendefinisikan struct dari transaksi
@@ -7,27 +7,65 @@ class Transaction:
     curr_op : pointer untuk mengakses operasi dan untuk rollback
     '''
 
-    def __init__(self, op: list, time: int):
+    def __init__(self, id: int, op: str, res: str):
+        self.id = id
         self.op = op
-        self.time = time
-        self.curr_op = 0
+        self.res = res
+
+    def pr():
+        print('test')
+        return (f'id:{self.id} op:{self.op} res:{self.res}')
 
 
-class Op:
-    # Enum untuk membedakan r/w/c
-    R = 'read'
-    W = 'write'
-    C = 'commit'
+R = 'R'
+W = 'W'
+C = 'C'
 
 
-def createOp(op: Op, res: str):
-    return {op: op, res: res}
+def createOp(op: str, res: str):
+    return {'op': op, 'res': res}
+
+
+def createTransaction():
+    n_commit = 0
+    tmp = []
+    trans_list = []
+    is_fin = False
+    while (not (is_fin)):
+        inp = input('specify operations (R/W/C) ex. R1: ')
+        opr = inp[0]
+        id = int(inp[1])
+        if (not(opr == C)):
+            res = input('specify resource for operations: ')
+        if (id not in trans_list):
+            trans_list.append(id)
+        if (opr == R or opr == W):
+            tmp.append(Transaction(id=id, op=opr, res=res))
+        elif (opr == C):
+            n_commit += 1
+            tmp.append(Transaction(id=id, op=C, res='commit'))
+
+        else:
+            print('invalid operations! ')
+        if(len(trans_list) == n_commit):
+            is_fin = True
+
+    return tmp, trans_list
+
+
+def prettyPrint(t: list):
+    for x in t:
+        if (x.op == C):
+            print("C" + str(x.id) + "->", end="")
+        else:
+            print(f'{x.op}{str(x.id)}({x.res})->', end="")
 
 
 # Asumsi operasi yang dilakukan t1 akan terurut dari indeks 0 hingga akhir
-op = [{Op.R: 'A'}, {Op.R: 'B'}, {Op.W: 'B'}]
+x, y = createTransaction()
 
-t1 = Transaction(op, 1)
+for el in x:
+    print(f'id:{el.id} op:{el.op} res:{el.res}')
+print(y)
 
-for x in t1.op:
-    print(x)
+prettyPrint(x)
